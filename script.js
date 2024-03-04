62ae6443580313ba90d9b31e6243547c796b9503
hemant modi <142676792+Hemant-1993@users.noreply.github.com>
2024-03-04 13:24:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function fibonacci(num) {
 // your code here
+	 if (num <= 1) {
+    return num;
+  }
+  const fib = [0, 1];
+  for (let i = 2; i <= num; i++) {
+    fib[i] = fib[i - 1] + fib[i - 2];
+  }
+  return fib[num];
 }
 
 module.exports = fibonacci;
